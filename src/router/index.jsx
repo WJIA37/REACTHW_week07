702bd3c512bc90ee42d48d94fc1414c95f425c0e
WJIA37 <r810307@yahoo.com.tw>
2025-03-14 02:28:03 +0800

    新增第七週作業

---
+++ src/router/index.jsx
@@ -0,0 +1,64 @@
+import { createHashRouter } from "react-router-dom";
+import FrontLayout from "../layouts/FrontLayout";
+import HomePages from "../pages/front/Homepage";
+import ProductsPage from "../pages/front/ProductsPage";
+import CartPage from "../pages/front/CartPage";
+import ProductsDetailPage from "../pages/front/ProductsDetailPage";
+import NotFound from "../pages/front/NotFound";
+import AdminLoginPage from "../pages/AdminLoginPage";
+import AdminLayout from "../layouts/AdminLayout";
+import AdminOrdersPage from "../pages/admin/AdminOrdersPage";
+import AdminProductsPage from "../pages/admin/AdminProductsPage";
+
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <FrontLayout />,
+    children: [
+      {
+        path: '',
+        element: <HomePages />,
+      },
+      {
+        path: 'products',
+        element: <ProductsPage />,
+      },{
+        path: 'products/:id',
+        element: <ProductsDetailPage />,
+      },
+      {
+        path: 'cart',
+        element: <CartPage />,
+      },
+    ],
+  },
+  {
+    path: 'adminLogin',
+    element: <AdminLoginPage />,
+  },
+  {
+    path: '/admin/login',
+    element: <AdminLayout />,
+    children:[
+      {
+        path:'',
+        element:<AdminLoginPage />,
+      },
+      {
+        path:'orders',
+        element:<AdminOrdersPage />,
+      },
+      {
+        path:'products',
+        element:<AdminProductsPage />,
+      }
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  },
+])
+
+export default router;
